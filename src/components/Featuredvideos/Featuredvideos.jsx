0acb9e84b1655ed9cccb041aa40ccb0d501ecadf
Prateek883 <singhprateek089@gmail.com>
2022-08-19 00:04:04 +0530

    install dependencies

--- src/components/Featuredvideos/Featuredvideos.jsx
+++ src/components/Featuredvideos/Featuredvideos.jsx
@@ -7,15 +7,26 @@ import styles from "./Featured.module.css";
 
 import VideoPlayer from "../VideoPlayer/VideoPlayers";
 
-
 function FeaturedVideos() {
-
-
   return (
     <div className={styles.ContentContainer}>
-      <h2 className={styles.newsletterTitle}>Featured Videos</h2>
+      <h2 className={styles.newsletterTitle}>Explore Videos</h2>
       <Row sm={12}>
-        <Col xs={12} sm={12}>
+      <Col xs={12} sm={4}>
+          <Card className={styles.subscribeCardBody}>
+            <Card.Body>
+              <VideoPlayer />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={4}>
+          <Card className={styles.subscribeCardBody}>
+            <Card.Body>
+              <VideoPlayer />
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={4}>
           <Card className={styles.subscribeCardBody}>
             <Card.Body>
               <VideoPlayer />
--- src/components/VideoPlayer/VideoPlayers.jsx
+++ src/components/VideoPlayer/VideoPlayers.jsx
@@ -10,8 +10,8 @@ function VideoPlayer() {
         <YoutubePlayer
                 src="https://www.youtube.com/watch?v=fkbm5nx-M9E"
                 allowFullScreen
-                height={500}
-                width={1300}
+                height={300}
+                width={370}
             />
         </div>
       </div>
